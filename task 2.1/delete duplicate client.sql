b069c678fd23d9fb79e2d85a35bd9c4939f7b039
ostdari <115879627+ostdari@users.noreply.github.com>
2024-08-04 15:18:24 +0300

    Update delete duplicate client.sql

--- task 2.1/delete duplicate client.sql
+++ task 2.1/delete duplicate client.sql
@@ -1,15 +1,18 @@
-
-WITH ranked_clients AS (
-    SELECT 
-           client_rk, 
-           effective_from_date,
-           ROW_NUMBER() OVER (
-               PARTITION BY client_rk, effective_from_date 
-           ) AS rn
-    FROM dm.client
+with ranked_clients as (
+select
+	client_rk,
+	effective_from_date,
+	row_number() over (
+               partition by client_rk,
+	effective_from_date 
+           ) as rn
+from
+	dm.client
 )
-DELETE FROM dm.client c
-USING ranked_clients rc
-WHERE c.client_rk=rc.client_rk and 
+delete from dm.client c
+using ranked_clients rc
+where
+	c.client_rk = rc.client_rk
+	and 
            c.effective_from_date = rc.effective_from_date
-AND rc.rn > 1;
+	and rc.rn > 1;
